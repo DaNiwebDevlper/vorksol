023a7adf5430e3f7a788192dd7c9cfe6ebd7882f
Danish <dnazir343@gmail.com>
2025-01-09 23:21:42 +0500

    .

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,33 +1,38 @@
 import React from 'react';
 
 const tableData = [
-    { image: "/assets/img1.png", clubName: "Athlele Ready", date: "12/12/2021", time: "12:00", distance: "2.5 km", menu: "/assets/menu.png" },
-    { image: "/assets/img2.png", clubName: "Club Name", date: "12/12/2021", time: "12:00", distance: "2.5 km", menu: "/assets/menu.png" },
-    { image: "/assets/img2.png", clubName: "Club Name", date: "12/12/2021", time: "12:00", distance: "2.5 km", menu: "/assets/menu.png" },
-    { image: "/assets/img2.png", clubName: "Club Name", date: "12/12/2021", time: "12:00", distance: "2.5 km", menu: "/assets/menu.png" },
+    { image: "/assets/img1.png", clubName: "Athlete Ready", date: "12 June, 2024", time: "06:20", distance: "Deloitte Rotterdam · 12Km", menu: "/assets/menu.png" },
+    { image: "/assets/img2.png", clubName: "Athlete Ready", date: "12 June, 2024", time: "06:20", distance: "Deloitte Rotterdam · 12Km", menu: "/assets/menu.png" },
+    { image: "/assets/img1.png", clubName: "Athlete Ready", date: "12 June, 2024", time: "06:20", distance: "Deloitte Rotterdam · 12Km", menu: "/assets/menu.png" },
+    { image: "/assets/img2.png", clubName: "Athlete Ready", date: "12 June, 2024", time: "06:20", distance: "Deloitte Rotterdam · 12Km", menu: "/assets/menu.png" },
 ];
 
 const List = () => {
     return (
-        <div className='flex flex-col gap-y-5 mt-5'>
-            <table className='w-full'>
+        <div className="bg-[#111] p-9 my-5 rounded-lg">
+            <table className="w-full table-auto">
                 <thead>
-                    <tr className='text-[#A5A5A5] border-b border-white/30'>
-                        <th></th>
-                        <th>Club Name</th>
-                        <th>Date</th>
-                        <th>Time</th>
-                        <th>Distance</th>
+                    <tr className="text-[#A5A5A5] border-b border-white/30 text-left">
+                        <th className="w-[15%]"></th>
+                        <th className="py-4">Club Name</th>
+                        <th className="py-4">Date</th>
+                        <th className="py-4">Time</th>
+                        <th className="py-4">Distance</th>
+
                     </tr>
                 </thead>
-                <tbody className='text-center space-y-4' >
+                <tbody className='text-left space-y-4 ' >
                     {tableData.map((data, index) => (
-                        <tr key={index} className='text-white bg-[#1C1C1C] border border-white/10'>
-                            <td className=''><img src={data.image} alt="club1" className='w-[100px]  object-cover' /></td>
+                        <tr key={index} className='text-white  bg-[#1C1C1C] border-b border-white/10'>
+                            <td className=''><img src={data.image} alt="club1" className='w-[100px]  object-cover ml-2' /></td>
                             <td className='align-middle '>{data.clubName}</td>
                             <td className='align-middle '>{data.date}</td>
                             <td className='align-middle '>{data.time}</td>
-                            <td className='align-middle '>{data.distance}</td>
+
+                            <td className="py-4 flex items-center justify-between pr-9">
+                                <span>{data.distance}</span>
+                                <img src={data.menu} alt="menu" />
+                            </td>
                         </tr>
                     ))}
                 </tbody>
